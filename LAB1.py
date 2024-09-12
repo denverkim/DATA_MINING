948d7e9b5a8c638f5ebc60a452514e42ae29a9dd
denverkim <Hyo-Jeong.Kim@hotmail.com>
2024-09-12 12:59:32 +0900

    Update LAB1.py

--- LAB1.py
+++ LAB1.py
@@ -10,7 +10,7 @@ This is a temporary script file.
 print('Hello, Python!')
 print("Python is a really great language, ", "isn't it?")
 
-#Twinkle 예제
+# Twinkle 예제
 print('Twinkle, twinkle, little star,')
 print('\tHow I wonder what you are!')
 print('\t\tUp above the world so high,')
@@ -18,7 +18,7 @@ print('\t\tLike a diamond in the sky.')
 print('Twinkle, twinkle, little star,')
 print('\tHow I wonder what you are!')
 
-#세금계산프로그램
+# 세금계산프로그램
 rate = .2
 std_ded = 10000
 dep_ded = 3000
@@ -27,3 +27,17 @@ income = float(input('Enter the gross income: '))
 num_dep = int(input('Enter the number of dependents: '))
 tax = (income - std_ded - dep_ded * num_dep)*rate
 print('The income tax is $' + str(tax))
+
+# 100살이 되는 년도 계산
+name = input("What is your name?")
+age = input("How old are you?")
+year = 100 - int(age) + 2018
+print(name + " will be 100 years old in the year " + str(year))
+
+# BMI 계산
+weight = input("what is your weight?")
+height = input("what is your height?")
+bmi = int(weight) / int(height)**2
+print("your BMI is " + "%6.4f"% bmi)
+#print("your BMI is " + str(bmi))
+
